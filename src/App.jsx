3e027744cc02911dc57d88338e1de0b3f24be5de
Mithil <mithilabhiram404@gmail.com>
2025-10-10 21:35:04 +0530

    API introduction with Async load

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Search from './components/Search.jsx';
 
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}` 
+  }
+}
 
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMovies = async () => {
+    try{
+
+    }catch(error) {
+      console.log(`Error Fetching Movies: ${error}`);
+      setErrorMessage(`Error Fetching Movies: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+
+  }, []);
 
   return (
     <main>
@@ -14,9 +38,14 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <section className='all-movies'>
+          <h2>All Movies</h2>
+        </section>
+
+        
         <h1 className='text-center text-white'>{searchTerm}</h1>
       </div>
     </main>
