4e794175f5397456b9a6fc3abb8cd23f9dafeb20
Mithil <mithilabhiram404@gmail.com>
2025-10-13 21:10:01 +0530

    Set Loading, Movie list display

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import Search from './components/Search.jsx';
+import Spinner from './components/Spinner.jsx';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -16,8 +17,14 @@ const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
 
   const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+
     try{
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
@@ -29,9 +36,20 @@ const App = () => {
       const data = await response.json();
       console.log(data);
 
+      if(data.response === 'False'){
+        setErrorMessage(data.Error || 'Failed to fetch the movies');
+        setMovieList([]);
+        return;
+      }
+      
+      setMovieList(data.results || []);
+
     } catch(error) {
       console.log(`Error Fetching Movies: ${error}`);
       setErrorMessage(`Error Fetching Movies: ${error}`);
+    
+    } finally{
+      setIsLoading(false);
     }
   }
 
@@ -51,9 +69,19 @@ const App = () => {
         </header>
 
         <section className='all-movies'>
-          <h2>All Movies</h2>
+          <h2 className="mt-[40px]">All Movies</h2>
+          {isLoading ? (
+          <p className="text-white"><Spinner/></p>
+          ) : errorMessage ? (
+          <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <p key={movie.id} className='text-white'>{movie.title}</p>
+              ))}
+          </ul>
+          )}
         </section>
-        {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
         
         <h1 className='text-center text-white'>{searchTerm}</h1>
       </div>
