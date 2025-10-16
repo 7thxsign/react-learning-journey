d280e6fa591b7317a6f415fca4f9d48c9f9fc1cc
Mithil <mithilabhiram404@gmail.com>
2025-10-16 21:18:18 +0530

    Debounce, search query and appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React,{useState, useEffect} from 'react';
 import Search from './components/Search.jsx';
 import Spinner from './components/Spinner.jsx';
 import MovieCard from './components/MovieCard.jsx';
+import {useDebounce} from 'react-use';
+import {updateSearchCount} from './appwrite.js';
 
 import './App.css';
 
@@ -23,14 +25,19 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
+  useDebounce(() => {
+    setDebouncedSearchTerm(searchTerm);
+  }, 500, [searchTerm]);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage('');
 
     try{
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+                  : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
 
       if(!response.ok){
@@ -48,6 +55,8 @@ const App = () => {
       
       setMovieList(data.results || []);
 
+      updateSearchCount();
+
     } catch(error) {
       console.log(`Error Fetching Movies: ${error}`);
       setErrorMessage(`Error Fetching Movies: ${error}`);
@@ -58,8 +67,8 @@ const App = () => {
   }
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   return (
     <main>
